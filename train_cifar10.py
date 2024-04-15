cbd905ef117bf4e8dbe7db2d939ce5739b56315d
Jiankun Cong <71331342+Sanqi-37-21@users.noreply.github.com>
2024-04-15 10:41:40 +0100

    Add files via upload

--- train_cifar10.py
+++ train_cifar10.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+import torchvision
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 from torchvision.utils import save_image, make_grid
@@ -14,33 +15,78 @@ from mindiffusion.unet import NaiveUnet
 from mindiffusion.ddpm import DDPM
 
 
+import time
+import psutil
+import os
+import subprocess
+
+#reform the picture
+img_size = 64
+batch_size = 16
+train_path = "./front"
+train_path2 = "./Abstract_gallery_2"
+
+def load_trans_dataset():
+    data_trans = [
+        transforms.Resize((img_size,img_size)),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda t:(t*2)-1)
+    ]
+
+    tran = transforms.Compose(data_trans)
+    # load first dataset
+    train = torchvision.datasets.ImageFolder(root=train_path,
+                                             transform=tran)
+    # load second dataset
+    train2 = torchvision.datasets.ImageFolder(root=train_path2,
+                                              transform=tran)
+    # this will combin the datasets together
+    return torch.utils.data.ConcatDataset([train,train2])
+
 def train_cifar10(
-    n_epoch: int = 100, device: str = "cuda:0", load_pth: Optional[str] = None
+    n_epoch: int = 1001, device: str = "cuda:0", load_pth: Optional[str] = None
 ) -> None:
 
     ddpm = DDPM(eps_model=NaiveUnet(3, 3, n_feat=128), betas=(1e-4, 0.02), n_T=1000)
 
-    if load_pth is not None:
-        ddpm.load_state_dict(torch.load("ddpm_cifar.pth"))
+    # if load_pth is not None:
+    #     ddpm.load_state_dict(torch.load("ddpm_cifar.pth"))
 
     ddpm.to(device)
 
-    tf = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    )
+    # tf = transforms.Compose(
+    #     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    # )
+    #
+    # dataset = CIFAR10(
+    #     "./data",
+    #     train=True,
+    #     download=True,
+    #     transform=tf,
+    # )
+    #
+    # dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2)
 
-    dataset = CIFAR10(
-        "./data",
-        train=True,
-        download=True,
-        transform=tf,
-    )
+    data = load_trans_dataset()
+    dataloader = DataLoader(data, batch_size=batch_size,
+                            shuffle=True, drop_last=True)
 
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2)
-    optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
+    optim = torch.optim.Adam(ddpm.parameters(), lr=0.001)
 
-    for i in range(n_epoch):
-        print(f"Epoch {i} : ")
+    # epochs = 5 # Try more!
+    epochs = [1, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 400, 500, 600, 700, 800, 900, 1000]  # Try more!
+    # epochs = [1] # Try more!
+
+    timelsit = []
+    gpulist = []
+    # for i in epochs:
+    time_start = time.time()
+
+    # m_state_dict = torch.load(f'train_cifar10weightPT/1000_train_cifar10.pt')
+    # ddpm.load_state_dict(m_state_dict)
+
+    for epoch in range(n_epoch):
+        print(f"Epoch {epoch} : ")
         ddpm.train()
 
         pbar = tqdm(dataloader)
@@ -59,13 +105,42 @@ def train_cifar10(
 
         ddpm.eval()
         with torch.no_grad():
-            xh = ddpm.sample(8, (3, 32, 32), device)
-            xset = torch.cat([xh, x[:8]], dim=0)
-            grid = make_grid(xset, normalize=True, value_range=(-1, 1), nrow=4)
-            save_image(grid, f"./contents/ddpm_sample_cifar{i}.png")
+            # xh = ddpm.sample(1, (3, 64, 64), device)
+            # xset = torch.cat([xh], dim=0)
+            # grid = make_grid(xset, normalize=True, value_range=(-1, 1), nrow=1)
+            # save_image(grid, f"./contents/ddpm_sample_cifar{epoch+1000}.png")
+            if epoch % 100 == 0:
+                print("start sampling")
+                for i in range(10):
+                    xh = ddpm.sample(1, (3, 64, 64), device)
+                    xset = torch.cat([xh], dim=0)
+                    grid = make_grid(xset, normalize=True, value_range=(-1, 1), nrow=1)
+                    save_image(grid, f"./train_cifar10PT_out/testOutput{epoch}/testOutput{epoch}/ddpm_sample_cifar{i}.png")
+                    print(f"save {i} image")
 
-            # save model
-            torch.save(ddpm.state_dict(), f"./ddpm_cifar.pth")
+            if epoch in epochs:
+                # # save trained data from the model
+                # torch.save(ddpm.state_dict(), f'./train_cifar10weightPT/{epoch}_train_cifar10.pt')
+
+                time_end = time.time()
+                time_sum = time_end - time_start
+                print(time_sum)
+                timelist.append(time_sum)
+
+                # using nvidia-smi command get GPU memory
+                result = subprocess.run(
+                    ['nvidia-smi', '--query-gpu=memory.total,memory.used', '--format=csv,noheader,nounits'],
+                    stdout=subprocess.PIPE)
+                # decode the output
+                output = result.stdout.decode('utf-8')
+
+                # get the memory used
+                for line in output.strip().split('\n'):
+                    total, used = line.split(', ')
+                    gpulist.append({"total_memory_MB": int(total), "used_memory_MB": int(used)})
+
+            print(timelist)
+            print(gpulist)
 
 
 if __name__ == "__main__":
--- train_cifar10_tf.py
+++ train_cifar10_tf.py
@@ -11,38 +11,30 @@ import matplotlib.pyplot as plt
 import time
 import tensorflow as tf
 from tensorflow.keras import Model
-import tensorflow_datasets as tfds
 from PIL import Image
+import psutil
+import os
+import subprocess
 
 from mindiffusion.unettf import NaiveUnet
 from mindiffusion.ddpmtf import DDPM
 
+
 def saveImage(images, filename, nrow=4):
-    # image 0 - 1
-    # images = ((images + 1.0) / 2.0) * 255
-    # images = tf.clip_by_value(images, 0, 255)
-    # images = tf.cast(images, tf.uint8)
-    # if len(images.shape) == 4:
-    #     # Remove the batch dimension if it's there
-    #     images = images[0]
-    # # change to numpy can use by plt
-    # images = images.numpy()
-    # # 4*4
-    # fig, axes = plt.subplots(nrows=nrow, ncols=nrow, figsize=(nrow, nrow))
-    # # make it can use for loop
-    # axes = axes.flatten()
-    images = images * [0.5, 0.5, 0.5] + [0.5, 0.5, 0.5]
-    image = tf.clip_by_value(image, 0, 255)
-    image = tf.cast(image, tf.uint8)
+    images = (images + 1) * 127.5
+    images = tf.clip_by_value(images, 0, 255)
+    images = tf.cast(images, tf.uint8)
     # change to numpy can use by plt
     images = images.numpy()
-    # 4*4
-    fig, axes = plt.subplots(nrows=nrow, ncols=nrow, figsize=(nrow, nrow))
+    # have nrow * nrow form of images
+    fig, axes = plt.subplots(nrows=nrow, ncols=nrow, figsize=(nrow, nrow), dpi=64)
     # make it can use for loop
-    axes = axes.flatten()
+    if nrow == 1:
+        axes = [axes]
+    else:
+        axes = axes.flatten()
     # go trough all the pic
     for img, ax in zip(images, axes):
-        # delete additional 1 in tensor and the color is in gray
         ax.imshow(img)
         ax.axis('off')
     # remove all the space between pictures
@@ -50,56 +42,123 @@ def saveImage(images, filename, nrow=4):
     plt.savefig(filename)
     plt.close()
 
+
+train_path = pathlib.Path('./front')
+# reform the picture
+img_size = 64
+batch_size = 16
+BUFFER_SIZE = 1000
+
+def load_and_transform_dataset(train_path):
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
+        train_path,
+        image_size=(img_size, img_size))
+
+    # Define a function to normalize images to [-1, 1]
+    def normalize(image,x):
+        image = (tf.cast(image, tf.float32) / 127.5) - 1
+        return image
+    # apply normalization
+    train_dataset = train_dataset.map(normalize, tf.data.AUTOTUNE)
+    # apply shuffle
+    train_dataset = train_dataset.shuffle(1000).prefetch(tf.data.AUTOTUNE)
+
+    return train_dataset
+
 def train_cifar10(
-    n_epoch: int = 100, device: str = "cuda:1", load_pth: Optional[str] = None
+    n_epoch: int = 1001, device: str = "/GPU:0", load_pth: Optional[str] = None
 ) -> None:
 
     ddpm = DDPM(eps_model=NaiveUnet(3, 3, n_feat=128), betas=(1e-4, 0.02), n_T=1000)
 
-    if load_pth is not None:
-        ddpm.load_state_dict(torch.load("ddpm_cifar.pth"))
+    # load data
+    dataset = load_and_transform_dataset(train_path)
+    dataset = dataset.apply(tf.data.experimental.unbatch())
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-    def normalize(image, x):
-        image = tf.cast(image, tf.float32) / 255.0
-        image = (image - [0.5, 0.5, 0.5]) / [0.5, 0.5, 0.5]
-        return image, x
-
-    dataset, dataset_info = tfds.load(
-        'cifar10',
-        split='train',
-        shuffle_files=True,
-        as_supervised=True,
-        with_info=True,
-        data_dir="./data"
-    )
-    dataset = dataset.map(normalize)
-    # change to smaller batch 好像tensorflow对于gpu的利用效率要低
-    dataset = dataset.batch(64, drop_remainder=True)
+    #optimizet
     optim = tf.keras.optimizers.Adam(learning_rate=1e-5)
 
-    for i in range(n_epoch):
+    # epochs = 5 # Try more!
+    epochs = [1, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 400, 500, 600, 700, 800, 900, 1000]  # Try more!
+
+
+    timelsit = []
+    gpulist = []
+    # for i in epochs:
+    time_start = time.time()
+    # saving model
+    # checkpoint = tf.train.Checkpoint(optimizer=optim, model=ddpm)
+    # restore = checkpoint.restore(f'train_cifar10weightTF/1_superminddpm_TF/1_train_cifar10.ckpt-1')
+    for epoch in range(n_epoch):
         # if Path("./ddpm_mnist.ckpt").exists():
         #     checkpoint.restore("./ddpm_mnist.ckpt")
         pbar = tqdm(dataset)
         loss_ema = None
-        for x, _ in pbar:
+        for x in pbar:
+            # pbar.set_description(f"Epoch {epoch}")
+            # traning
             with tf.GradientTape() as tape:
+                # get loss
                 loss = ddpm(x)
+                # loss function
                 if loss_ema is None:
                     loss_ema = loss[0]
                 else:
                     loss_ema = 0.9 * loss_ema + 0.1 * loss[0]
-                pbar.set_description(f"loss: {loss_ema:.4f}")
+                # progress bar
+                pbar.set_description(f"Epoch {epoch} | loss: {loss_ema:.4f}")
+            # get gradient
             gradients = tape.gradient(loss_ema, ddpm.trainable_variables)
+            # apply gradient
             optim.apply_gradients(zip(gradients, ddpm.trainable_variables))
+        # sampling images
+        if epoch % 100 == 0:
+            print("start sampling")
+            for i in range(10):
+                xh = ddpm.sample(1, (64, 64, 3), device)
+                xset = tf.concat([xh], axis=0)
+                saveImage(xset, f"./train_cifar10TF_out/testOutput{epoch}/testOutput{epoch}/ddpm_sample_cifar{i}.png", nrow=1)
+                print(f"save {i} image")
+        # sampling images
+        if epoch % 1000 == 0:
+            xh = ddpm.sample(8, (64, 64, 3), device)
+            xset = tf.concat([xh, x[:8]], axis=0)
+            saveImage(xset, f"./train_cifar10TF_out/ddpm_sample_cifar{epoch+10}.png")
 
-        xh = ddpm.sample(8, (32, 32, 3), device)
-        xset = tf.concat([xh, x[:8]], axis=0)
-        saveImage(xset, f"./contents/ddpm_sample_cifar{i}.png")
 
-        # save model
-        # checkpoint.save(f"./ddpm_mnist.ckpt")
+        if epoch in epochs:
+            # save trained data from the model
+            # checkpoint = tf.train.Checkpoint(optimizer=optim, model=ddpm)
+            # checkpoint.save(file_prefix=f'train_cifar10weightTF/{epoch}_superminddpm_TF/{epoch}_train_cifar10.ckpt')
 
 
+            if epoch in epochs:
+                # # save trained data from the model
+                # checkpoint = tf.train.Checkpoint(optimizer=optim, model=ddpm)
+                # checkpoint.save(file_prefix=f'superweightTF/{epoch}_superminddpm_TF/{epoch}_superminddpm_TF.ckpt')
+
+                time_end = time.time()
+                time_sum = time_end - time_start
+                print(time_sum)
+                timelist.append(time_sum)
+
+                # using nvidia-smi command get GPU memory
+                result = subprocess.run(
+                    ['nvidia-smi', '--query-gpu=memory.total,memory.used', '--format=csv,noheader,nounits'],
+                    stdout=subprocess.PIPE)
+                # decode the output
+                output = result.stdout.decode('utf-8')
+
+                # get the memory used
+                for line in output.strip().split('\n'):
+                    total, used = line.split(', ')
+                    gpulist.append({"total_memory_MB": int(total), "used_memory_MB": int(used)})
+
+            print(timelist)
+            print(gpulist)
+
+# runnning
 if __name__ == "__main__":
     train_cifar10()
